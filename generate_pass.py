6fb59a1760455ad2657dd9d7054ec98f63a1c7d6
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-08-09 15:06:40 -0300

    create password function

--- generate_pass.py
+++ generate_pass.py
@@ -10,4 +10,8 @@ def create_password_list(lists, empty_list):
         empty_list.append(i)
 
 
-
+def create_password(password_list):
+    password = ""
+    for i in password_list:
+        password += i
+    return password
