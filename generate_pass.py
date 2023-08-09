8663df2f67a5dd993cf83bfeea149e87081c02fa
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-08-09 15:06:15 -0300

    create password list function

--- generate_pass.py
+++ generate_pass.py
@@ -5,4 +5,9 @@ def randomize_input(lists, inputs):
     return random.choices(lists, k=inputs)
 
 
+def create_password_list(lists, empty_list):
+    for i in lists:
+        empty_list.append(i)
+
+
 
