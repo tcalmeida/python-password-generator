e41dddbb1a23a01a2490fcd2d00b7e28b9cf135b
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-08-09 15:07:30 -0300

    create characters lists

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+from generate_pass import randomize_input, create_password_list, create_password
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'K', 'L', 'M', 'N', 'O', 'P']
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '+']
+
+
