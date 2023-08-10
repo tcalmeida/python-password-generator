deff7e198f83f5269d680cd80779fd9fd4ebd4e9
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-08-10 10:37:51 -0300

    import chars module

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import random
+from chars import letters, numbers, symbols
 from generate_pass import randomize_input, create_password_list, create_password
-
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'K', 'L', 'M', 'N', 'O', 'P']
-numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '+']
 
 
 def main():
